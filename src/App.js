34fab2fc79f906dc2115ad8c09316d6e583ba5d7
muhammad-danish1998 <iamdanish1998@gmail.com>
2020-06-05 00:40:30 +0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         i am Muhammad Danish :)
+         please like and subscribe my channel :)
         </p>
         <a
           className="App-link"
